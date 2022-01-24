9c0cade33dac2d803c7b46c20a760f1d5a66e6c1
Lory Costa <77644367+lory-costa@users.noreply.github.com>
2022-01-24 17:32:09 +1300

    build: remove svg plugin to use svg directly

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,6 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /images/,
-        },
-      },
-    },
     "gatsby-plugin-postcss",
     // ...
   ],
