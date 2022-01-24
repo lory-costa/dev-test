060368a66138c0071358a99c5712ef31b636aa92
Lory Costa <77644367+lory-costa@users.noreply.github.com>
2022-01-25 10:39:14 +1300

    fix: dropdown menu to work properly for mobile

--- src/components/menuDropdown.js
+++ src/components/menuDropdown.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const MenuDropdown = () => {
   return (
-    <div className='h-[100vh] fixed top-0 bg-white z-10 flex items-center mt-12 xl:h-auto xl:relative'>
+    <div className='flex justify-start flex-col-reverse lg:flex-row w-full fixed lg:relative z-10 top-0 pt-20 lg:pt-0 -ml-5 lg:ml-0 bg-white items-center mt-12 lg:place-content-around'>
       <img
-        width='180'
-        height='180'
+        width='210'
+        height='210'
         src='https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-180x180.png'
-        alt=''
+        alt='Women figures'
         loading='lazy'
         srcset='https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-180x180.png 180w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-700x700.png 700w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-100x100.png 100w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-768x768.png 768w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-240x240.png 240w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-360x360.png 360w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-480x480.png 480w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-640x640.png 640w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-300x300.png 300w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-150x150.png 150w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle-160x160.png 160w, https://1m6n143uhgz333i25i4am7ul-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/Group-Final_circle.png 800w'
         sizes='(max-width: 180px) 100vw, 180px'
-        className='attachment-180x180 size-180x180 mr-8'
+        className='mt-5 lg:mt-0'
       />
       <div className='text-center'>
         <h3 className='text-2xl mb-3'>
