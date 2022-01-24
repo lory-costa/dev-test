60d3e8f804baa0cf3f9aa5a6b1b4733bdd51f377
Lory Costa <77644367+lory-costa@users.noreply.github.com>
2022-01-25 11:58:34 +1300

    refactor: improve responsiveness

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,15 +3,15 @@ import React from "react";
 const Hero = () => {
   return (
     <div className='flex flex-col items-center text-darkBlue'>
-      <div className='text-7xl my-4 items-center flex flex-col'>
+      <div className='text-4xl lg:text-7xl my-4 items-center text-center flex flex-col'>
         <h1>Tech needs you.</h1>
         <h1>Just as you are.</h1>
       </div>
-      <p className='mb-6 text-center'>
+      <p className='mb-6 text-sm text-center'>
         Join Girls in Tech to discover inspiring programs and a diverse
         community designed to help you <br /> enter into, and excel in, tech.
       </p>
-      <div>
+      <div className='flex items-center'>
         <button className='border-2 border-orange bg-white hover:bg-outlineHover text-darkBlue py-2 px-10 rounded-full font-light mr-1'>
           About Girls In Tech
         </button>
